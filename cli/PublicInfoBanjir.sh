4960d8e34c2bc35cfb908906694bdeb0ad5fdb3c
Ahmad Malik <arma7x@live.com>
2023-04-28 20:07:13 +0800

    fix path

--- cli/PublicInfoBanjir.sh
+++ cli/PublicInfoBanjir.sh
@@ -7,12 +7,12 @@ done < <(php ./PublicInfoBanjir/StateList)
 
 function printRainLevel() {
   read -p "Masukan kod negeri: " STATE
-  echo $STATE | xargs ./PublicInfoBanjir/RainLevel
+  echo $STATE | xargs ./cli/PublicInfoBanjir/RainLevel
 }
 
 function printRiverLevel() {
   read -p "Masukan kod negeri: " STATE
-  echo $STATE | xargs ./PublicInfoBanjir/RiverLevel
+  echo $STATE | xargs ./cli/PublicInfoBanjir/RiverLevel
 }
 
 printf "\r\nPilih kod operasi:\r\n"
